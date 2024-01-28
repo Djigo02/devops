044ed5ee77e53ef66e57b8acec9f58762d48b230
Mouhamad Djigo <99468961+Djigo02@users.noreply.github.com>
2024-01-28 01:25:37 +0000

    Add files via upload

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline{
+    agent any
+    stages{
+        stage('Recuperation du projet'){
+            steps{
+                echo 'Recuperation'
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        if(a > b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
